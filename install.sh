989037fa8319cde0847ab673c8aa471f822d89cb
logank <logan.aker1989@gmail.com>
2024-08-29 10:53:39 -0400

    minor path changes

--- install.sh
+++ install.sh
@@ -15,12 +15,14 @@ sudo apt install -y lsd tmux glances ranger toilet bpytop net-tools neofetch zsh
 
 # .configs
 NeofetchConfig="files/config/NEOFETCH.conf"
-mkdir -p .config/neofetch/ && mv $NeofetchConfig .config/neofetch/config.conf
+mkdir -p ~/.config/neofetch/ && mv $NeofetchConfig ~/.config/neofetch/config.conf
 
 # /etc/ssh/
 SSHConfig="files/SSH_CONFIG"
 mv $SSHConfig /etc/ssh/ssh_config
-figlet -f slant $HOST >>file.txt
+figlet $HOST >>file.txt
+mv file.txt /etc/ssh/
+
 # .tmux.conf
 TmuxConfig="files/TMUX.conf"
 mv $TmuxConfig ~/.tmux.conf
