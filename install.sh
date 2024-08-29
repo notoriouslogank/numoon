684c37baaa70feb3393331d6f7e5a2935fc4116e
logank <logan.aker1989@gmail.com>
2024-08-29 10:32:04 -0400

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+## DEBIAN
+
+# installs
+#sudo apt update && sudo apt full-upgrade -y
+#sudo apt install lsd tmux glances ranger toilet bpytop net-tools neofetch
+
+# .configs
+SSHConfig="files/ssh_config"
+echo $SSHConfig
+cat $SSHConfig
+# /etc/ssh/
+
+# .tmux.conf
+
+# zshrc
