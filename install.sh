7eec3f85f4bba67c5a550ec8f9be47b37abbe60c
logank <logan.aker1989@gmail.com>
2024-08-29 10:34:45 -0400

    Made all refs relative.

--- install.sh
+++ install.sh
@@ -2,16 +2,29 @@
 
 ## DEBIAN
 
+# backups
+mkdir backups
+sudo cp /etc/ssh/ssh_config ./backups/ssh_config
+sudo cp ~/.zshrc ./backups/zshrc
+sudo cp ~/.bashrc ./backups/bashrc
+sudo cp ~/.tmux.conf ./backups/tmux.conf
+
 # installs
-#sudo apt update && sudo apt full-upgrade -y
-#sudo apt install lsd tmux glances ranger toilet bpytop net-tools neofetch
+sudo apt update && sudo apt full-upgrade -y
+sudo apt install lsd tmux glances ranger toilet bpytop net-tools neofetch
 
 # .configs
-SSHConfig="files/ssh_config"
-echo $SSHConfig
-cat $SSHConfig
+NeofetchConfig="files/config/NEOFETCH.conf"
+mkdir -p .config/neofetch/ && mv $NeofetchConfig .config/neofetch/config.conf
+
 # /etc/ssh/
+SSHConfig="files/SSH_CONFIG"
+mv $SSHConfig /etc/ssh/ssh_config
 
 # .tmux.conf
+TmuxConfig="files/TMUX.conf"
+mv $TmuxConfig ~/.tmux.conf
 
 # zshrc
+Zshrc="files/ZSHRC"
+mv $Zshrc ~/.zshrc
